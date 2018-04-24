d47a6540ae99cffd03e6d092b4be02fad3a66e40
zone320 <zone320@gmail.com>
2018-04-24 05:52:19 -0500

    Added new solutions

---
+++ zone320.Recipe.DataAccess/zone320.Recipe.DataAccess/Base/BaseDA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zone320.Recipe.DataAccess.Base
+{
+    public class BaseDA
+    {
+        //TODO:
+    }
+}
